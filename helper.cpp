28c22f3bb91a1db42083b81ff7d07379eaca343f
dreiiiiiiiiiii <161920929+dreiiiiiiiiiii@users.noreply.github.com>
2025-09-18 01:40:56 +0800

    feat: add main command loop and help command

---
+++ helper.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "helper.h"
+
+
+void showHelp() {
+    std::cout <<
+        "Commands:\n"
+        "   help            Show this help\n"
+        "   start_marquee   Start marquee\n"
+        "   stop_marquee    Stop marquee\n"
+        "   set_text        Set marquee text\n"
+        "   set_speed       Set marquee speed\n"
+        "   exit            Exit program\n";
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,33 @@
 #include "helper.h"
 
 int main() {
+    std::string command;
     
+    std::cout<<"Welcome to CSOPESY!"<<std::endl;
+
+    std::cout<<"Group Developer:"<<std::endl;
+
+    std::cout<<"Version Date:"<<std::endl;
+    
+    while (true) {
+        std::cout<<"Command>";
+        std::cin>>command;
+        if (command == "help") {
+            showHelp();
+        } else if (command == "start_marquee") {
+            // start marquee
+        } else if (command == "stop_marquee") {
+            // stop marquee
+        } else if (command == "set_text") {
+            // set text
+        } else if (command == "set_speed") {
+            // set speed
+        } else if (command == "exit") {
+            std::cout << "Exiting program." << std::endl;
+            return 0;
+        } else {
+            std::cout<<"Command not found"<<std::endl;
+        }
+    }
     return 0;
 }
